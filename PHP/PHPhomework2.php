4713ebcf3174b7d240fd92cf6df68979a304e144
toru828 <82481957+toru828@users.noreply.github.com>
2021-04-19 17:33:08 +0900

    Update PHPhomework2.php

--- PHP/PHPhomework2.php
+++ PHP/PHPhomework2.php
@@ -2,11 +2,11 @@
   
 function factorialOfNumber($n)
 {
-  $x = 1;
-  for ($i = 1; $i <= $n; $i++) {
-   $x *= $i;
-  }
-  return $x;
+    $x = 1;
+    for ($i = 1; $i <= $n; $i++) {
+       $x *= $i;
+    }
+    return $x;
 }
 
 echo factorialOfNumber(10);
