ad551c7fe7e05153b87e05fada762db92ae5beb2
toru828 <82481957+toru828@users.noreply.github.com>
2021-04-19 17:34:16 +0900

    Update PHPhomework3.php

--- PHP/PHPhomework3.php
+++ PHP/PHPhomework3.php
@@ -2,17 +2,17 @@
    
 function primeNumberFinder($n)
 {
-  $x = 0;
-  $y = 1;
-  for ($i = 2; $i < $n ; $i++) {
-   $x = $n % $i;
-   $y *= $x;
-  }
-  if($y == 0) {
-   echo "The number is not prime number.";
-  } else {
-   echo "The number is prime number.";
-  }
+    $x = 0;
+    $y = 1;
+    for ($i = 2; $i < $n ; $i++) {
+        $x = $n % $i;
+        $y *= $x;
+    }
+    if($y == 0) {
+        echo "The number is not prime number.";
+        } else {
+        echo "The number is prime number.";
+        }
 }
 primeNumberFinder();
 ?>
