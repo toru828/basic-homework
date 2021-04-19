8f38b9e5fbe0942bb04bf3ab4c25c62360c581c3
toru828 <82481957+toru828@users.noreply.github.com>
2021-04-19 22:56:28 +0900

    Update PHPhomework4.php

--- PHP/PHPhomework4.php
+++ PHP/PHPhomework4.php
@@ -4,7 +4,7 @@ function findPrimeNumberBetween($n)
     for ($m = 2; $m <= $n; $m++) {
         $x = 0;
         $y = 1;
-        for ($i = 2; $i < $m ; $i++) {
+        for ($i = 2; $i < $m; $i++) {
             $x = $m % $i;
             $y *= $x;
         }
