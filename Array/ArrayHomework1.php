e59b9fd2e27cffc8f64d5079931be69700fb09bf
toru828 <82481957+toru828@users.noreply.github.com>
2021-04-22 17:01:48 +0900

    Update ArrayHomework1.php

--- Array/ArrayHomework1.php
+++ Array/ArrayHomework1.php
@@ -1,12 +1,12 @@
 <?php
+
 $calculate = array(5, 12, 17, 9, 3);
-
-
 
 $avg = 0;
 $sum = 0;
 $min = $calculate[0];
 $max = $calculate[0];
+
 for ($i = 0; $i < 5; $i++) {
     $sum += $calculate[$i];
     $avg = $sum / 5;
@@ -17,6 +17,7 @@ for ($i = 0; $i < 5; $i++) {
         $max = $calculate[$i];
     }
 }
+
 echo "average is " . $avg . "<br>";
 echo "sum is " . $sum . "<br>";
 echo "min is " . $min . "<br>";
