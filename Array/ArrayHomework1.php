58d14b4e6c96460312f66c857e3424f3b2324a6b
toru828 <82481957+toru828@users.noreply.github.com>
2021-04-22 17:01:19 +0900

    Update ArrayHomework1.php

--- Array/ArrayHomework1.php
+++ Array/ArrayHomework1.php
@@ -1,30 +1,25 @@
 <?php
-$calculate = array(
-    array(5, 12, 17, 9, 3),
-    array(13, 4, 8, 14, 1),
-    array(9, 5, 3, 7, 21)
-);
+$calculate = array(5, 12, 17, 9, 3);
 
 
 
-for ($i = 0; $i < 3; $i++) {
-    $avg = 0;
-    $sum = 0;
-    $min = $calculate[0][0];
-    $max = $calculate[0][0];
-    for ($j = 0; $j < 5; $j++) {
-        $sum += $calculate[$i][$j];
-        $avg = $sum / 5;
-        if ($min > $calculate[$i][$j]) {
-            $min = $calculate[$i][$j];
-        }
-        if ($max < $calculate[$i][$j]) {
-            $max = $calculate[$i][$j];
-        }
+$avg = 0;
+$sum = 0;
+$min = $calculate[0];
+$max = $calculate[0];
+for ($i = 0; $i < 5; $i++) {
+    $sum += $calculate[$i];
+    $avg = $sum / 5;
+    if ($min > $calculate[$i]) {
+        $min = $calculate[$i];
     }
-    echo "group " . $i . " average is " . $avg . "<br>";
-    echo "group " . $i . " sum is " . $sum . "<br>";
-    echo "group " . $i . " min is " . $min . "<br>";
-    echo "group " . $i . " max is " . $max . "<br>";
+    if ($max < $calculate[$i]) {
+        $max = $calculate[$i];
+    }
 }
+echo "average is " . $avg . "<br>";
+echo "sum is " . $sum . "<br>";
+echo "min is " . $min . "<br>";
+echo "max is " . $max . "<br>";
+
 ?>
