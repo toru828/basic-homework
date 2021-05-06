8965c9cee6a293f21402188b287cfb54c05acf8d
toru828 <82481957+toru828@users.noreply.github.com>
2021-05-06 13:45:36 +0900

    Update LinkedListHomework1.php

--- Data Structure/Linked List/LinkedListHomework1.php
+++ Data Structure/Linked List/LinkedListHomework1.php
@@ -80,7 +80,7 @@ class LinkedList {
 
     public function deleteAll($data)
     {
-        if ($this->head === null) { // linked list is empty
+        if (is_null($this->head)) { // linked list is empty
             echo "List is empty.";
             return;
         }
