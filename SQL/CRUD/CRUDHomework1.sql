10e0ed91ab71474b6af42bf4bf55c55d3337755f
toru828 <82481957+toru828@users.noreply.github.com>
2021-05-19 14:26:47 +0900

    Update CRUDHomework1.sql

--- SQL/CRUD/CRUDHomework1.sql
+++ SQL/CRUD/CRUDHomework1.sql
@@ -1,5 +1,5 @@
 CREATE TABLE `new_agents` AS
-SELECT `agents`.`AGENT_CODE`, `agents`.`AGENT_NAME`, `agents`.`WORKING_AREA`, `agents`.`COMMISSION`, `agents`.`PHONE_NO`, `agents`.`COUNTRY`
+SELECT DISTINCT `agents`.`AGENT_CODE`, `agents`.`AGENT_NAME`, `agents`.`WORKING_AREA`, `agents`.`COMMISSION`, `agents`.`PHONE_NO`, `agents`.`COUNTRY`
 FROM `agents`
 JOIN `customer`
 ON `agents`.`AGENT_CODE` = `customer`.`AGENT_CODE`
