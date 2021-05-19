c478410ebb2ac16aef9bfb21a18d2715584e7b34
toru828 <82481957+toru828@users.noreply.github.com>
2021-05-19 14:49:24 +0900

    Update CRUDHomework2.sql

--- SQL/CRUD/CRUDHomework2.sql
+++ SQL/CRUD/CRUDHomework2.sql
@@ -4,11 +4,6 @@ WHERE `agents`.`AGENT_CODE` = (
 	SELECT `customer`.`AGENT_CODE`
     FROM `customer`
     GROUP BY `customer`.`AGENT_CODE`
-    HAVING SUM(`customer`.`PAYMENT_AMT`) = (
-		SELECT SUM(`customer`.`PAYMENT_AMT`)
-        FROM `customer`
-        GROUP BY `customer`.`AGENT_CODE`
-        ORDER BY SUM(`customer`.`PAYMENT_AMT`) ASC
-        LIMIT 1
-    )
+    ORDER BY SUM(`customer`.`PAYMENT_AMT`) ASC
+    LIMIT 1
 )
