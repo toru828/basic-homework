8f1b554d7bf724e532d649e0defea6386b6315b8
toru828 <82481957+toru828@users.noreply.github.com>
2021-05-20 14:53:26 +0900

    Update SQLBestPracticeHomework.sql

--- SQL/SQLBestPractice/SQLBestPracticeHomework.sql
+++ SQL/SQLBestPractice/SQLBestPracticeHomework.sql
@@ -1,10 +1,10 @@
 SELECT
-	`working_area`, avg(`commission`), count(`agent_name`)
+	`working_area`, avg(`commission`) AS `avg_commission`, count(`agent_name`) AS `count_agent_name`
 FROM
 	`agents`
 GROUP BY
 	`working_area`
 HAVING
-	count(`agent_name`) < 3
+	`count_agent_name` < 3
 ORDER BY
-	avg(`commission`), count(`agent_name`) desc;
+	`avg_commission`, `count_agent_name` desc;
