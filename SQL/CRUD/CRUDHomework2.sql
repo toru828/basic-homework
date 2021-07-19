7258af68e2ed602345e30caaf9a05daf778b6f2f
toru828 <82481957+toru828@users.noreply.github.com>
2021-07-19 15:02:47 +0900

    Update CRUDHomework2.sql

--- SQL/CRUD/CRUDHomework2.sql
+++ SQL/CRUD/CRUDHomework2.sql
@@ -1,7 +1,7 @@
 UPDATE `agents`
 SET COMMISSION=COMMISSION-.02
 WHERE `agents`.`AGENT_CODE` = (
-	SELECT `customer`.`AGENT_CODE`
+    SELECT `customer`.`AGENT_CODE`
     FROM `customer`
     GROUP BY `customer`.`AGENT_CODE`
     ORDER BY SUM(`customer`.`PAYMENT_AMT`) ASC
